68113e135f8107dcb865e7a7ba46e80a2a9cd50f
LingHan.S <LINGKUI0324@GMAIL.COM>
2024-01-17 12:55:52 +1000

    Update header spacing and add hero image
    Refined the site header by adding a top margin for improved visual separation. Replaced the placeholder text in the main page with a hero image to enhance visual interest. Carousel component code is prepared but commented out, likely for future use. Additionally, introduced the 'embla-carousel-react' package as a dependency to enable carousel functionality later on.
    
    Closes #42 with the styling improvements and sets the groundwork for #56, the carousel feature implementation.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
 import Image from 'next/image'
 import Header from './Header'
+import { Carousels } from './Caiousel'
 
 export default function Home() {
   return (
-    <main>
+    <main className=''>
       <Header />
-      <div>Carousel</div>
+      <div className=' flex w-full justify-center items-center my-6'>
+        {/* <Carousels /> */}
+        <Image
+          src='/assets/hero.jpg '
+          alt='Paroro Logo'
+          sizes='100vw'
+          style={{
+            width: '100%',
+            height: 'auto',
+          }}
+          width={500}
+          height={300}
+        />
+      </div>
       <div>Find Out More</div>
       <div>Location</div>
       <div>Offerings</div>
