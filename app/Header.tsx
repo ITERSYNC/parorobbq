609bb25b799d3373ed81ac1fe0c6ff9714d93789
LingHan.S <LINGKUI0324@GMAIL.COM>
2024-01-17 11:23:56 +1000

    init

---
+++ app/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-vw bg-black flex justify-around'>
+      <div className='flex'>
+        <div>HOME</div>
+        <div>ABOUT US</div>
+        <div>LOCATION</div>
+      </div>
+      <div>LOGO</div>
+      <div>CONTACT US</div>
+    </div>
+  )
+}
+
+export default Header
