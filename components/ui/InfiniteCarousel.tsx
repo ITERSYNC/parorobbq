de44f86668488d028d489b044559a073de640350
smarthulala <chao.long.au@gmail.com>
2024-03-04 22:12:53 +1100

    update infinite carousel with png file

--- components/ui/InfiniteCarousel.tsx
+++ components/ui/InfiniteCarousel.tsx
@@ -2,19 +2,17 @@
 import React, { useEffect, useRef } from 'react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import Image from 'next/image'
 
 gsap.registerPlugin(ScrollTrigger)
 gsap.defaults({ overwrite: true })
 
 function InfiniteCarousel() {
   const listItems = [
-    { name: 'buffet' },
-    { name: 'meat' },
-    { name: 'seafood' },
-    { name: 'all' },
-    { name: 'you' },
-    { name: 'can' },
-    { name: 'eat' },
+    { name: 'meat', image: '/assets/meat.png' },
+    { name: 'seafood', image: '/assets/seafood.png' },
+    { name: 'sushi', image: '/assets/sushi.png' },
+    { name: 'buffet', image: '/assets/bbq.png' },
   ]
 
   const extendedList = [...listItems, ...listItems, ...listItems, ...listItems]
@@ -37,7 +35,7 @@ function InfiniteCarousel() {
   useEffect(() => {
     gsap
       .to('.rightToLeft', {
-        xPercent: -150,
+        xPercent: -100,
         duration: 45,
         ease: 'none',
         repeat: -1,
@@ -49,27 +47,34 @@ function InfiniteCarousel() {
     <div className='flex justify-center items-center'>
       <div className='lg:text-[60px] text-[40px] font-extrabold uppercase overflow-hidden'>
         <ul className='leftToRight flex text-nowrap gap-5 md:gap-20'>
-          {extendedList.map(({ name }, index) => (
-            <li
-              key={index}
-              className={`${
-                (name === 'you' || name === 'can' || name === 'eat') &&
-                '-ml-3 md:-ml-16'
-              }`}
-            >
+          {extendedList.map(({ name, image }, index) => (
+            <li key={index} className='flex items-center justify-center'>
+              {image && (
+                <Image
+                  src={image}
+                  alt={`${name} icon`}
+                  className='inline-block mr-2'
+                  width={50}
+                  height={50}
+                />
+              )}
+
               {name}
             </li>
           ))}
         </ul>
         <ul className='rightToLeft flex text-nowrap gap-5 md:gap-20'>
-          {extendedList.map(({ name }, index) => (
-            <li
-              key={index}
-              className={`${
-                (name === 'you' || name === 'can' || name === 'eat') &&
-                '-ml-3 md:-ml-16'
-              }`}
-            >
+          {extendedList.map(({ name, image }, index) => (
+            <li key={index}>
+              {image && (
+                <Image
+                  src={image}
+                  alt={`${name} icon`}
+                  className='inline-block mr-2'
+                  width={50}
+                  height={50}
+                />
+              )}
               {name}
             </li>
           ))}
