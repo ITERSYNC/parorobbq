cbb0d385aa6980118455ed2d625e940a94ac0517
smarthulala <chao.long.au@gmail.com>
2024-03-04 18:31:21 +1100

    fix ui bug

--- components/ui/InfiniteCarousel.tsx
+++ components/ui/InfiniteCarousel.tsx
@@ -11,68 +11,55 @@ function InfiniteCarousel() {
     { name: 'buffet' },
     { name: 'meat' },
     { name: 'seafood' },
-    { name: 'all you can eat' },
+    { name: 'all' },
+    { name: 'you' },
+    { name: 'can' },
+    { name: 'eat' },
   ]
 
-  // useEffect(() => {
-  //   gsap.fromTo(
-  //     '.leftToRight',
-  //     {
-  //       xPercent: -150,
-  //     },
-  //     {
-  //       xPercent: 0,
-  //       ease: 'none',
-  //       duration: 30,
-  //       repeat: -1,
-  //     }
-  //   )
-  // }, [])
+  useEffect(() => {
+    gsap.fromTo(
+      '.leftToRight',
+      {
+        xPercent: -150,
+      },
+      {
+        xPercent: 0,
+        ease: 'none',
+        duration: 30,
+        repeat: -1,
+      }
+    )
+  }, [])
 
-  // useEffect(() => {
-  //   gsap
-  //     .to('.rightToLeft', {
-  //       xPercent: -150,
-  //       duration: 30,
-  //       ease: 'none',
-  //       repeat: -1,
-  //     })
-  //     .timeScale(1)
-  // }, [])
+  useEffect(() => {
+    gsap
+      .to('.rightToLeft', {
+        xPercent: -150,
+        duration: 30,
+        ease: 'none',
+        repeat: -1,
+      })
+      .timeScale(1)
+  }, [])
 
   return (
     <div className='flex justify-center items-center'>
       <div className='lg:text-[60px] text-[20px] font-extrabold uppercase overflow-hidden'>
-        <ul className='leftToRight flex'>
+        <ul className='leftToRight flex text-nowrap gap-20'>
           {listItems.map(({ name }) => {
-            return (
-              <li key={name} className='text-nowrap'>
-                {name}
-              </li>
-            )
+            return <li key={name}>{name}</li>
           })}
           {listItems.map(({ name }) => {
-            return (
-              <li key={name} className='text-nowrap'>
-                {name}
-              </li>
-            )
+            return <li key={name}>{name}</li>
           })}
         </ul>
-        <ul className='rightToLeft flex'>
+        <ul className='rightToLeft flex text-nowrap gap-20'>
           {listItems.map(({ name }) => {
-            return (
-              <li key={name} className='text-nowrap'>
-                {name}
-              </li>
-            )
+            return <li key={name}>{name}</li>
           })}
           {listItems.map(({ name }) => {
-            return (
-              <li key={name} className='text-nowrap'>
-                {name}
-              </li>
-            )
+            return <li key={name}>{name}</li>
           })}
         </ul>
       </div>
