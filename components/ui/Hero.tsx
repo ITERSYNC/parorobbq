4f14a695927d6563e33072a620b6cc92786a5739
smarthulala <chao.long.au@gmail.com>
2024-03-04 20:22:59 +1100

    new bg image for phone view

--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -64,7 +64,7 @@ export default function Hero() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-between relative px-0 mt-[100px] bg-[url('/assets/heroBackground.jpeg')] bg-fixed bg-left-bottom">
+    <div className="flex flex-col items-center justify-between relative px-0 mt-[100px] bg-[url('/assets/heroBackgroundSmall.jpg')] md:bg-[url('/assets/heroBackground.jpeg')] bg-fixed bg-cover">
       <div className='mx-auto'>
         <div className='grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center '>
           <div className='col-start-1 md:row-start-1 px-10 lg:px-6 md:px-4'>
