4274de7235d4033023f87cccb9077fdd95df0235
smarthulala <chao.long.au@gmail.com>
2024-03-14 20:01:58 +1100

    add picture and ui to session and pricing

--- app/FindOutMore.tsx
+++ app/FindOutMore.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+'use client'
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import { Separator } from '@/components/ui/separator'
+import { Divide } from 'lucide-react'
 
 function FindOutMore() {
+  const [isPhoneView, setIsPhoneView] = useState(false)
+
+  useEffect(() => {
+    function handleResize() {
+      setIsPhoneView(window.innerWidth <= 640)
+    }
+
+    handleResize() // Initial check
+    window.addEventListener('resize', handleResize) // Listen to resize events
+
+    return () => {
+      window.removeEventListener('resize', handleResize) // Clean up event listener
+    }
+  }, [])
+
   return (
     <div className='text-center flex justify-center py-14 bg-white'>
       <div className='w-4/5 ipadV:w-1/2 flex flex-col gap-10 items-center'>
@@ -13,52 +30,101 @@ function FindOutMore() {
           Notice: Lunch session start from 18/03/2024
         </h3>
 
-        <div className='text-black flex gap-10'>
-          <div className='text-nowrap leading-loose'>
-            <div>
-              <h1 className='text-[#caad66] font-bold text-xl md:text-2xl'>
-                Lunch
-              </h1>
-              <h2>
-                Time: <span className='font-bold'>12:00-14:30</span>
-              </h2>
-              <p>
-                Adult: <span className='font-bold'>$19.90</span>
-              </p>
-              <p>
-                Kid(4-10): <span className='font-bold'>$11.9</span>
-              </p>
-              <p>Including: </p>
-              <p>Korean Hot Food</p>
-              <p>Fruit and Salad</p>
+        {isPhoneView ? (
+          <div className='text-black flex flex-col gap-10'>
+            <div className='text-nowrap leading-loose flex gap-5'>
+              <div className='text-left'>
+                <h1 className='text-[#caad66] text-center font-bold text-3xl md:text-3xl'>
+                  Lunch
+                </h1>
+                <h2>
+                  Time: <span className='font-bold'>12:00-14:30</span>
+                </h2>
+                <p>
+                  Adult: <span className='font-bold'>$19.90</span>
+                </p>
+                <p>
+                  Kid(4-10): <span className='font-bold'>$11.9</span>
+                </p>
+                <p>Including: </p>
+                <p>Korean Hot Food</p>
+                <p>Fruit and Salad</p>
+              </div>
+              <div className="bg-[url('/assets/lunchpicture.jpg')] w-[187px] h-[250px] bg-contain bg-no-repeat shadow-2xl rounded"></div>
             </div>
-            <div></div>
-          </div>
-          <Separator orientation='vertical' className='bg-black h-full' />
+            <Separator orientation='horizontal' className='bg-black' />
 
-          <div className='text-nowrap leading-loose'>
-            <div></div>
-            <div>
-              <h1 className='text-[#caad66] font-bold text-xl md:text-2xl'>
-                Dinner
-              </h1>
-              <h2>
-                Time: <span className='font-bold'>17:00-22:30</span>
-              </h2>
-              <p>
-                Adult: <span className='font-bold'>$49.90</span>
-              </p>
-              <p>
-                Kid(4-10): <span className='font-bold'>$29.9</span>
-              </p>
-              <p>Including: </p>
-              <p>Korean BBQ</p>
-              <p>Sushi</p>
-              <p>Korean Hot Food</p>
-              <p>Fruit and Salad</p>
+            <div className='text-nowrap leading-loose flex gap-5'>
+              <div className="bg-[url('/assets/dinnerpicture.jpg')] w-[187px] h-[250px] bg-contain bg-no-repeat bg-center bg-black shadow-2xl rounded"></div>
+              <div className='text-left'>
+                <h1 className='text-[#caad66] text-center font-bold text-3xl md:text-3xl'>
+                  Dinner
+                </h1>
+                <h2>
+                  Time: <span className='font-bold'>17:00-22:30</span>
+                </h2>
+                <p>
+                  Adult: <span className='font-bold'>$49.90</span>
+                </p>
+                <p>
+                  Kid(4-10): <span className='font-bold'>$29.9</span>
+                </p>
+                <p>Including: </p>
+                <p>Korean BBQ</p>
+                <p>Sushi</p>
+                <p>Korean Hot Food</p>
+                <p>Fruit and Salad</p>
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <div className='text-black flex gap-10'>
+            <div className='text-nowrap leading-loose flex gap-5'>
+              <div className="bg-[url('/assets/lunchpicture.jpg')] w-[187px] h-[250px] bg-contain bg-no-repeat shadow-2xl rounded"></div>
+              <div className='text-left'>
+                <h1 className='text-[#caad66] text-center font-bold text-3xl md:text-3xl'>
+                  Lunch
+                </h1>
+                <h2>
+                  Time: <span className='font-bold'>12:00-14:30</span>
+                </h2>
+                <p>
+                  Adult: <span className='font-bold'>$19.90</span>
+                </p>
+                <p>
+                  Kid(4-10): <span className='font-bold'>$11.9</span>
+                </p>
+                <p>Including: </p>
+                <p>Korean Hot Food</p>
+                <p>Fruit and Salad</p>
+              </div>
+            </div>
+            <Separator orientation='vertical' className='bg-black' />
+
+            <div className='text-nowrap leading-loose flex gap-5'>
+              <div className='text-left'>
+                <h1 className='text-[#caad66] text-center font-bold text-3xl md:text-3xl'>
+                  Dinner
+                </h1>
+                <h2>
+                  Time: <span className='font-bold'>17:00-22:30</span>
+                </h2>
+                <p>
+                  Adult: <span className='font-bold'>$49.90</span>
+                </p>
+                <p>
+                  Kid(4-10): <span className='font-bold'>$29.9</span>
+                </p>
+                <p>Including: </p>
+                <p>Korean BBQ</p>
+                <p>Sushi</p>
+                <p>Korean Hot Food</p>
+                <p>Fruit and Salad</p>
+              </div>
+              <div className="bg-[url('/assets/dinnerpicture.jpg')] w-[187px] h-[250px] bg-contain bg-no-repeat bg-center bg-black shadow-2xl rounded"></div>
+            </div>
+          </div>
+        )}
 
         <div className=' text-gray-400'>
           <p>
