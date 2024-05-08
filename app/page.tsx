c1182b1152e4fec0d7acef5b84f8ab1fce6e9ad8
LingHan.S <LINGKUI0324@GMAIL.COM>
2024-05-08 15:10:58 +1000

    Update hero section in page.tsx to display new pricing information

--- app/page.tsx
+++ app/page.tsx
@@ -578,6 +578,18 @@ export default function Example() {
             />
 
             <h1 className='font-permanent-marker text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
+              Pricing <br />
+              Lunch AU$ 19.90 <br />
+              Dinner AU$ 49.90 <br />
+              <Separator
+                orientation='horizontal'
+                className='my-3 h-[2px] bg-gray-900 sm:h-1'
+              />
+              <span className='font-nanum-brush text-[25px] sm:text-3xl'>
+                Monday - Friday
+              </span>
+            </h1>
+            <h1 className='font-permanent-marker text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
               ALL DAY 10% OFF
               <br />
               <span className='font-nanum-brush text-[25px] sm:text-3xl'>
