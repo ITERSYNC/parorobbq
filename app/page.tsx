e79f1aedffcc4f586b71924dc00fc56d6ff56571
LingHan.S <LINGKUI0324@GMAIL.COM>
2024-09-24 08:54:58 +1000

    Update hero section in page.tsx to adjust padding and improve responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -558,7 +558,7 @@ export default function Example() {
       </header>
 
       <div className='relative isolate px-6 pt-14 lg:px-8'>
-        <div className='mx-auto max-w-2xl pb-28 pt-16 lg:py-56'>
+        <div className='mx-auto max-w-2xl pb-8 pt-16 lg:py-36'>
           {/* <div className='hidden sm:mb-8 sm:flex sm:justify-center'>
             <div className='relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20'>
               Announcing our next round of funding.{' '}
